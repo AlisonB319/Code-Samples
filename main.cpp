b093113b0791b4bc40008b1c57029872a74fb55a
AlisonB319 <32000834+AlisonB319@users.noreply.github.com>
2017-12-12 18:28:41 -0800

    Create main.cpp
    main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include"PuttingPiecesTogether.h"
+
+/*******************************************************************************
+* Programmer: Alison Burgess                                                   *
+* Class: CptS 122; Lab Section 1                                               *
+* Programming Assignment: PA 5                                                *
+* Date: 7-12-17                                                                *
+*                                                                              *
+* Description: Creates a BST of english letters and Morse Code symbols         *
+* then reads from a file and creates a english to morse code translation       *
+*                                                                              *
+* Relevant Formulas: Refer to each function definition.                        *
+*                                                                              *
+* Format of record in input file ():                               char string * 
+* Format of output file (): BST                                                *
+*                           translation                                        *
+********************************************************************************/
+
+
+int main(void)
+{
+
+	PuttingThePiecesTogether();
+
+
+	return 0;
+}
+
